4712363867971494dd08e3eac152f41f1cf0284a
mosescode1 <119126909+Mosefa1@users.noreply.github.com>
2023-05-17 22:22:57 +0100

    first commit

---
+++ advice/advice.js
@@ -0,0 +1,17 @@
+// const quotes = [{
+//     quote: `"Work On Goals. Working on goals that matter first is among the best advice and tips anyone can give you. Give care to your assignments and show your boss you have enthusiasm for what you are doing.If the assignment is not something that excites you, motivate yourself with personal tasks to achieve a goal."`
+// }, {
+//     quote: `"Be Organized. Use a personal organization system and be organized with your daily tasks. This can help you develop good habits and make you well-organized. You will be surprised how much more you can do when you organize your daily schedule rather than just doing things on the fly."`
+// }, {
+//     quote: `"Be A Team Player. Do not be selfish. Everyone will appreciate a person who works well alongside other people rather than someone who works alone. This is one of the most significant pieces of advice and tips to become a successful leader in the future. Being a team player raises how people respect you, and at the same time, builds a strong network and relationship with your co-workers."`
+// }
+// ]
+
+// let btn = document.querySelector("#Qbtn");
+// let quote = document.querySelector("#.quote");
+
+// btn.addEventListener("click", function() {
+//     let random = Math.floor(Math.random() * quotes.length); 
+
+//     quote.innerHTML = quotes[random].quote;
+// })
---
+++ index.js
@@ -0,0 +1,30 @@
+function changeBg() {
+    var bar = document.getElementById('bar');
+    var scrollValue = window.scrollY;
+    if(scrollValue < 520) {
+        bar.classList.remove('header-scrolled');
+    }else{
+        bar.classList.add('header-scrolled');
+    }
+    console.log(scrollValue); 
+}
+
+window.addEventListener('scroll', changeBg)
+
+
+ $(document).ready(function () {
+   $("#icon").click(function () {
+     $("ul").toggleClass("show");
+   });
+ });
+
+
+
+
+// const header = document.querySelector('.header');
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 150) {
+//         header.classList.add('header-scrolled');
+//     }
+// })
